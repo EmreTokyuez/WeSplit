3ca29150af37f869d83dfb3df9ca5b79823b43ba
git@tokyuez.net <github@tokyuez.net>
2023-06-09 10:37:00 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Emre Tokyüz on 09.06.23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
